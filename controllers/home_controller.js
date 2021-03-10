c623db5c6cfdb3b4038a8136d4e613cf1e9bc6da
ashwiniJais <ashwani.jaiswal01@gmail.com>
2021-03-10 21:32:17 +0530

    added post form on home.ejs and display all the posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,10 +1,19 @@
+const Post=require('../models/posts');
+
+
 module.exports.home=function(req,res){
-  //  console.log(req.cookies);
-//    res.cookie('user_id',6969);
-//    console.log(req.cookies);
+
+  Post.find({},function(err,post){
+    if(err){
+      console.log("Error in fetching all the posts from DB", err);
+      return;
+    };
+
     return res.render('home',{
-        title:"SocialBuzz Home"
-    })
-    
-    //return res.end('<h1>Controller connected</h1>');
-}
+      title:"SocialBuzz | Home",
+      posts:post
+    });
+  
+  });
+
+};
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const homeController=require('../controllers/home_controller');
 console.log("router connected");
 
 router.use('/users',require('./users'));
+router.use('/posts',require('./posts'));
 router.get('/', homeController.home);
 
 module.exports=router;
