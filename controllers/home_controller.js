96e32d0ab526c5d40c05ebe2cc9cd6d1ef0cc4e0
ashwiniJais <ashwani.jaiswal01@gmail.com>
2021-03-10 21:46:30 +0530

    populating the user to see details of the user who made particular post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,17 +3,22 @@ const Post=require('../models/posts');
 
 module.exports.home=function(req,res){
 
-  Post.find({},function(err,post){
-    if(err){
-      console.log("Error in fetching all the posts from DB", err);
-      return;
-    };
-
-    return res.render('home',{
-      title:"SocialBuzz | Home",
-      posts:post
-    });
+  Post.find({}).
+  populate('user').
+  exec(function(err,post){
+    {
+      if(err){
+        console.log("Error in fetching all the posts from DB", err);
+        return;
+      };
   
-  });
+      return res.render('home',{
+        title:"SocialBuzz | Home",
+        posts:post
+      });
+    
+    }
+  })
+
 
 };
