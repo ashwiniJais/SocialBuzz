ebe531c5b210aaa8bbd8b51dfa03e96da22c8677
ashwiniJais <ashwani.jaiswal01@gmail.com>
2021-09-16 19:38:56 +0530

    connect to mongodb atla

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,27 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/SocialBuzz_dev');
 
-const db=mongoose.connection;
+// const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'Error in connecting to MongoDB'));
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error :${error.message}`);
+    process.exit(1);
+  }
+};
 
-db.once('open',function(){
-    console.log("Successfully connected to MongoDB");
-})
+// db.on('error', console.error.bind(console, 'Error in connecting to MongoDB'));
 
-module.exports=db;
+// db.once('open', function () {
+//   console.log('Successfully connected to MongoDB');
+// });
+
+module.exports = connectDB;
